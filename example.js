9ee7154039d52c79d1caa78282cc61b9c94d5aa8
Hinh1009 <50358382+Hinh1009@users.noreply.github.com>
2020-03-25 00:59:37 +0700

    Update mondai 6

--- example.js
+++ example.js
@@ -3,3 +3,20 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+arr = []
+
+for(var i=2;i < process.argv.length;i++){
+   arr.push(parseInt(process.argv[i])) 
+}
+
+for(var i=0; i <arr.length;i++){
+    for(var j= i+1 ; j<arr.length;j++){
+        if(arr[j]<arr[i]){
+            var tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+        }
+    }
+}
+
+console.log(arr[Math.floor(arr.length/2)])
